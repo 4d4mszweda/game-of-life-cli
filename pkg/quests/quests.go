f90c493dd0c7b5375ed7e3b65e7ba9ea08a4843c
Adam <maxsuspl@gmail.com>
2024-11-30 12:54:19 +0100

    ?

--- pkg/quests/quests.go
+++ pkg/quests/quests.go
@@ -1,6 +1,7 @@
 package quests
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -17,7 +18,47 @@ type questLine struct {
 }
 
 type quest struct {
-	title string
-	desc  string
-	check bool
+	title       string
+	description string
+	check       bool
+}
+
+// AddQuestLine adds a new questLine to the questLog.
+func (ql *questLog) AddQuestLine(name string) {
+	ql.mutex.Lock()
+	defer ql.mutex.Unlock()
+
+	*ql.questLines = append(*ql.questLines, questLine{
+		name:   name,
+		quests: []quest{},
+	})
+}
+
+// AddQuest adds a new quest to a specific questLine by name.
+func (ql *questLog) AddQuest(lineName string, newQuest quest) error {
+	ql.mutex.Lock()
+	defer ql.mutex.Unlock()
+
+	for i, line := range *ql.questLines {
+		if line.name == lineName {
+			(*ql.questLines)[i].quests = append(line.quests, newQuest)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("questLine '%s' not found", lineName)
+}
+
+// ListQuests lists all quests in a specific questLine by name.
+func (ql *questLog) ListQuests(lineName string) ([]quest, error) {
+	ql.mutex.Lock()
+	defer ql.mutex.Unlock()
+
+	for _, line := range *ql.questLines {
+		if line.name == lineName {
+			return line.quests, nil
+		}
+	}
+
+	return nil, fmt.Errorf("questLine '%s' not found", lineName)
 }
